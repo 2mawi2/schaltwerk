69c9b1d7cdf6d0d73ccb00f20789d756921dd1db
2mawi2 <2mawi2@gmail.com>
2025-11-15 16:20:52 +0100

    Remove @xterm/addon-ligatures

--- src/terminal/xterm/xtermAddonImporter.ts
+++ src/terminal/xterm/xtermAddonImporter.ts
@@ -1,6 +1,5 @@
 import type { FitAddon as FitAddonType } from '@xterm/addon-fit'
 import type { ImageAddon as ImageAddonType } from '@xterm/addon-image'
-import type { LigaturesAddon as LigaturesAddonType } from '@xterm/addon-ligatures'
 import type { SearchAddon as SearchAddonType } from '@xterm/addon-search'
 import type { SerializeAddon as SerializeAddonType } from '@xterm/addon-serialize'
 import type { Unicode11Addon as Unicode11AddonType } from '@xterm/addon-unicode11'
@@ -10,7 +9,6 @@ import type { WebLinksAddon as WebLinksAddonType } from '@xterm/addon-web-links'
 export interface IXtermAddonNameToCtor {
   fit: typeof FitAddonType
   image: typeof ImageAddonType
-  ligatures: typeof LigaturesAddonType
   search: typeof SearchAddonType
   serialize: typeof SerializeAddonType
   unicode11: typeof Unicode11AddonType
@@ -38,10 +36,6 @@ async function loadAddonCtor<T extends XtermAddonName>(name: T): Promise<IXtermA
       case 'image': {
         const module = await import('@xterm/addon-image')
         return module.ImageAddon as IXtermAddonNameToCtor[T]
-      }
-      case 'ligatures': {
-        const module = await import('@xterm/addon-ligatures')
-        return module.LigaturesAddon as IXtermAddonNameToCtor[T]
       }
       case 'search': {
         const module = await import('@xterm/addon-search')
